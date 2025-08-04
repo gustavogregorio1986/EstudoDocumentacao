54af915c2f439dec2b9d334637168b87fec88478
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-08-04 13:51:49 -0300

    Add project files.

---
+++ Estudo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Estudo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> fibonacciNumbers = [1, 1];
+
+        }
+    }
+}
---
+++ Estudo00/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> fibonacciNumbers = [1, 1];
+
+while(fibonacciNumbers.Count > 20)
+{
+    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+}
+
+foreach (var item in fibonacciNumbers)
+{
+    Console.WriteLine(item);
+}
