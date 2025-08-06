4424132f6365b0647099e7d76e99fae8a62a3ca8
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-08-06 17:46:47 -0300

    Estudando a sintaxe do C# para evoluir como profissional

---
+++ Estudo02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        StringBuilder builder = new();
+        builder.AppendLine("The following arguments are passed:");
+
+        foreach (var arg in args)
+        {
+            builder.AppendLine(arg);
+        }
+
+        Console.WriteLine(builder.ToString());
+
+        return 0; // Código de saída do programa
+    }
+}
