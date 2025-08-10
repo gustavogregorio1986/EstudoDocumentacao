a811696f6dd92ca87ab69f6d1f54cc564de76614
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-08-10 20:34:41 -0300

    Estudando sobre a documentação de C# com .net

---
+++ Estudo12/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Estudo12
+{
+    public record Person(string FirstName, string LastName);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new("Gustavo", "Kuzava");
+            Console.WriteLine(person);
+        }
+    }
+}
---
+++ Estudo13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Estudo13
+{
+    public record Person(string FirstName, string LastName, string[] phoneNumbers);
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneNumbers = new string[2]; 
+            Person person1 = new("John", "Doe", phoneNumbers);
+            Person person2 = new("Gustavo", "Kuzava", phoneNumbers);
+            Console.WriteLine(person1 == person2);
+
+            person1.phoneNumbers[0] = "123-456-7890";
+            Console.WriteLine(person1 == person2);
+
+            Console.WriteLine(ReferenceEquals(person1, person2));
+        }
+    }
+}
