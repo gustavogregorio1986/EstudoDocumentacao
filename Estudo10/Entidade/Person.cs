7be16bb5c05b19eed5f2b432b7a689e87cc29d1f
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-08-10 13:03:20 -0300

    Desenvolvimento dos codigos da documentação

---
+++ Estudo10/Entidade/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo10.Entidade
+{
+    public class Person
+    {
+        public required string LastName { get; set; }
+
+        public required string FirstName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName}  {LastName}";
+        }
+    }
+}
---
+++ Estudo10/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Estudo10.Entidade;
+
+//var p1 = new Person(); Erro pois teria que colocar valores para as propriedades LastName e FirstName
+
+var p2 = new Person
+{
+    LastName = "Silva",
+    FirstName = "João"
+};
+
+Console.WriteLine(p2);
