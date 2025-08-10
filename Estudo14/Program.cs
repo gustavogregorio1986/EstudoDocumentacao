8bb775b442a79e0e55e432033e919d8eb5dd81dc
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-08-10 20:39:25 -0300

    Estudo paar desnevolvimento profissional, da documentação na linguagem C#

---
+++ Estudo14/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Estudo14
+{
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
