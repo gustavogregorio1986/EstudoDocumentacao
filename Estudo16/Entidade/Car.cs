c686ba4adb69977c39db9b95fc5e06005c3e02ac
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-08-10 20:52:51 -0300

    Documentação do projeto C#, para aprender melhor!

---
+++ Estudo16/Entidade/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo16.Entidade
+{
+    public class Car : IEquatable<Car>
+    {
+        public string? Make { get; set; }
+
+        public string? Model { get; set; }
+
+        public string? Year { get; set; }
+
+        public bool Equals(Car car)
+        {
+            return(this.Make,this.Model,this.Year) ==
+            (car?.Make, car?.Model, car?.Year);
+        }
+    }
+}
---
+++ Estudo16/Interface/IEquatable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo16.Interface
+{
+    public interface IEquatable
+    {
+        bool Equals(object obj);
+    }
+}
