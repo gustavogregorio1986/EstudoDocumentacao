d999c7fb39bc337864e404016246c9ce0ff02506
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-08-10 21:01:44 -0300

    Desenvolvimento da documentação na linguagem C#

---
+++ Estudo17/TestGenericList.cs
@@ -0,0 +1,27 @@
+﻿using Estudo17;
+using System;
+
+namespace Estudo17
+{
+    public class GenericList<T>
+    { 
+          public void Add(T item) { }
+    }
+
+    public class ExampleClass { }
+
+    public class TestGenericList
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list1 = new();
+            list1.Add(1);
+
+            GenericList<string> list2 = new();
+            list2.Add("");
+
+            GenericList<ExampleClass> list3 = new();
+        list3.Add(new ExampleClass());
+    }
+    }
+}
